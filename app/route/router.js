a366a52209d45e10d71e8b44920d93cb56dfa13f
huseyinerdall <huseyinerdal@protonmail.com>
2020-11-02 21:18:34 +0300

    edit

--- app/route/router.js
+++ app/route/router.js
@@ -2,21 +2,12 @@ module.exports = function(app) {
 
     const currency = require('../controller/currency.controller.js');
 
-    // Create a new Customer
     app.post('/api/customer', currency.create);
 
-    // Retrieve all Customer
+    // Retrieve all Cuurencies
     app.get('/api/currency', currency.findAll);
 
     // Retrieve a single Customer by Id
-    app.get('/api/customer/:customerId', currency.findById);
-
-  // Retrieve Customers Age
-    app.get('/api/currency/age/:age', currency.findByAge);
-
-    // Update a Customer with Id
-    app.put('/api/customer/:customerId', currency.update);
-
-    // Delete a Customer with Id
-    app.delete('/api/customer/:customerId', currency.delete);
+    app.get('/api/customer/:currencyId', currency.findById);
+    
 }
