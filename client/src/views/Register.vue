0b7e3d0e77dd2201aa3b3ddf098c89d2b1d36418
huseyinerdall <huseyinerdal@protonmail.com>
2020-11-20 20:34:17 +0300

    flags added

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -104,7 +104,7 @@ export default {
   },
   methods:{
     register(){
-      axios.post(`${this.$store.state.addr}:${this.$store.state.port}/register`,{
+      axios.post(`http://${this.$store.state.addr}:${this.$store.state.port}/register`,{
         fullName: this.fullname,
         email: this.email,
         passwd: this.password1
